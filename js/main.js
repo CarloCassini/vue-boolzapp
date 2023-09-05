e8f2b24a00858eab46350d419df8c32b2c869888
CarloCassini <c.cassini87@gmail.com>
2023-09-05 17:43:39 +0200

    add orario

--- js/main.js
+++ js/main.js
@@ -1,4 +1,3 @@
-console.log("e andiamo");
 const DateTime = luxon.DateTime;
 const { createApp } = Vue;
 
@@ -34,8 +33,51 @@ createApp({
       setTimeout(this.otherMsg, 1000);
     },
 
+    formatDate() {
+      let dataFormattata = "";
+      // "10/01/2020 15:51:00"
+      const data = new Date();
+      let giorno = data.getDate();
+      if (data.getDate() < 10) {
+        giorno = "0" + giorno;
+      }
+      let mese = data.getMonth();
+      if (data.getMonth() < 10) {
+        mese = "0" + mese;
+      }
+      let anno = data.getFullYear();
+
+      let ore = data.getHours();
+      if (data.getHours() < 10) {
+        ore = "0" + data.getHours();
+      }
+      let minuti = data.getMinutes();
+      if (data.getMinutes() < 10) {
+        minuti = "0" + data.getMinutes();
+      }
+      let secondi = data.getSeconds();
+      if (data.getSeconds() < 10) {
+        secondi = "0" + data.getSeconds();
+      }
+
+      dataFormattata =
+        giorno +
+        "/" +
+        mese +
+        "/" +
+        anno +
+        " " +
+        ore +
+        ":" +
+        minuti +
+        ":" +
+        secondi;
+
+      return dataFormattata;
+    },
+
     mineMsg() {
-      this.newMessage.date = new Date();
+      this.newMessage.date = this.formatDate();
       this.newMessage.status = "sent";
       const newToAdd = { ...this.newMessage };
       this.contacts[this.activeContact].messages.push(newToAdd);
@@ -43,7 +85,9 @@ createApp({
       this.newMessage.status = "";
     },
     otherMsg() {
-      this.newMessage.date = new Date();
+      // "10/01/2020 15:51:00"
+
+      this.newMessage.date = this.formatDate();
       this.newMessage.message = "ok";
       this.newMessage.status = "received";
       const newToAdd = { ...this.newMessage };
@@ -68,15 +112,13 @@ createApp({
     },
 
     deleteMessage(index) {
-      console.log(this.contacts[this.activeContact].messages[index]);
       this.contacts[this.activeContact].messages.splice(index, 1);
     },
     findLast(user) {
       let lunghezza = user.messages.length - 1;
-      console.log(user.messages);
+
       while (user.messages[lunghezza].status == "sent" && lunghezza > 0) {
         lunghezza = lunghezza - 1;
-        console.log(lunghezza);
       }
 
       if (lunghezza < 0) {
